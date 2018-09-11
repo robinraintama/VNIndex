d4779c132a41ccceee1110a9235c46937de27f02
robinraintama <robinraintama@gmail.com>
2018-09-11 14:16:59 +0700

    Task 02-1,2,3,4
    Todo
    - Error handling
    - Error page
    - Index page

--- app/controllers/companies.py
+++ app/controllers/companies.py
@@ -13,8 +13,20 @@ ROOT_PATH = os.environ.get('ROOT_PATH')
 @app.route('/companies', methods=['GET'])
 def companies():
     ''' route read companies '''
-    if request.method == 'GET':
-        data = mongo.db.company_profile.find({},{'_id': 0})
-        data_lists = list(data)
-        data_response = json.dumps(data_lists, default=json_util.default)
-        return data_response, 200
+
+    find_param = {}
+    remove_id_param = {'_id': 0}
+
+    if request.args.get('company_name') is not None:
+    	find_param['company name'] = request.args.get('company_name')
+    if request.args.get('business') is not None:
+    	find_param['business'] = request.args.get('business')
+    if request.args.get('revenue_gte') is not None:
+    	revenue = int(request.args.get('revenue_gte'))
+    	find_param['financial summary.Market Cap'] = {'$gte':revenue}
+
+    print(find_param)
+    data = mongo.db.company_profile.find(find_param,remove_id_param)
+    data_lists = list(data)
+    data_response = json.dumps(data_lists, default=json_util.default)
+    return data_response, 200
